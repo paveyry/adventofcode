a0834ea16eb507394c19723a071eff9fbcb6d854
Pierre-Alexandre VEYRY <pa.veyry@gmail.com>
2023-12-01 23:15:40 +0000

    2023: d1: optimize code and make it more idiomatic

--- aoc2023/src/bin/d1.rs
+++ aoc2023/src/bin/d1.rs
@@ -1,53 +1,47 @@
-use std::io::{prelude::*, BufReader};
-use std::fs::File;
+use std::fs;
+use std::time::Instant;
 
-use anyhow::{Result, Error};
+use anyhow::{Error, Result};
 
-fn ex1(filename: &str) -> Result<u32> {
-    let file = File::open(filename)?;
-    let reader = BufReader::new(file);
+fn ex1(file: &str) -> Result<u32> {
     let mut sum = 0;
-    for line in reader.lines() {
-        let l = line?;
-        let mut num_str = String::new();
-        for c in l.chars() {
-            if c.is_numeric() {
-                num_str.push(c);
-                break;
-            }
-        }
-        for c in l.chars().rev() {
-            if c.is_numeric() {
-                num_str.push(c);
-                break;
-            }
-        }
-        sum += num_str.parse::<u32>()?
+    for l in file.lines() {
+
+        let u1 = l
+            .chars()
+            .find(|c| c.is_numeric())
+            .unwrap()
+            .to_digit(10)
+            .ok_or_else(|| Error::msg("bad digit"))?;
+        let u2 = l
+            .chars()
+            .rev()
+            .find(|c| c.is_numeric())
+            .unwrap()
+            .to_digit(10)
+            .ok_or_else(|| Error::msg("bad digit"))?;
+
+        sum += u1 * 10 + u2;
     }
     Ok(sum)
 }
 
-fn ex2(filename: &str) -> Result<u32> {
-    let file = File::open(filename)?;
-    let reader = BufReader::new(file);
+fn ex2(file: &str) -> Result<u32> {
     let mut sum = 0;
-    for line in reader.lines() {
-        let l = line?;
-        let mut num_str = String::new();
-        
-        for (i, c) in l.chars().enumerate() {
-            if let Some(num) = num_at_pos(&l, i, c) {
-                num_str += num.to_string().as_str();
-                break
-            }
-        }
-        for (i, c) in l.chars().rev().enumerate() {
-            if let Some(num) = num_at_pos(&l, l.len()-1-i, c) {
-                num_str += num.to_string().as_str();
-                break
-            }
-        }
-        sum += num_str.parse::<u32>()?
+    for l in file.lines() {
+
+        let u1 = l
+            .chars()
+            .enumerate()
+            .find_map(|(i, c)| num_at_pos(&l, i, c))
+            .unwrap();
+        let u2 = l
+            .chars()
+            .rev()
+            .enumerate()
+            .find_map(|(i, c)| num_at_pos(&l, l.len() - 1 - i, c))
+            .unwrap();
+        sum += u1 * 10 + u2;
     }
     Ok(sum)
 }
@@ -57,34 +51,65 @@ fn num_at_pos(s: &str, pos: usize, c: char) -> Option<u32> {
         return c.to_digit(10);
     }
     if pos >= 2 {
-        match &s[pos-2..=pos] {
-            "one" => { return Some(1); },
-            "two" => { return Some(2); },
-            "six" => { return Some(6); },
-            _ => {},
+        match &s[pos - 2..=pos] {
+            "one" => {
+                return Some(1);
+            }
+            "two" => {
+                return Some(2);
+            }
+            "six" => {
+                return Some(6);
+            }
+            _ => {}
         }
     }
     if pos >= 3 {
-        match &s[pos-3..=pos] {
-            "four" => { return Some(4); },
-            "five" => { return Some(5); },
-            "nine" => { return Some(9); },
-            _ => {},
+        match &s[pos - 3..=pos] {
+            "four" => {
+                return Some(4);
+            }
+            "five" => {
+                return Some(5);
+            }
+            "nine" => {
+                return Some(9);
+            }
+            _ => {}
         }
     }
     if pos >= 4 {
-        match &s[pos-4..=pos] {
-            "three" => { return Some(3); },
-            "seven" => { return Some(7); },
-            "eight" => { return Some(8); },
-            "nine" => { return Some(9); },
-            _ => { return None; },
+        match &s[pos - 4..=pos] {
+            "three" => {
+                return Some(3);
+            }
+            "seven" => {
+                return Some(7);
+            }
+            "eight" => {
+                return Some(8);
+            }
+            "nine" => {
+                return Some(9);
+            }
+            _ => {
+                return None;
+            }
         }
     }
     None
 }
 
 fn main() {
-    println!("ex1: {}", ex1("inputs/d1_1.txt").unwrap());
-    println!("ex2: {}", ex2("inputs/d1_1.txt").unwrap());
+    let file = fs::read_to_string("./inputs/d1_1.txt").unwrap();
+
+    let start = Instant::now();
+    let res_ex1 = ex1(&file);
+    let duration = start.elapsed();
+    println!("ex1: {} (computed in {:?})", res_ex1.unwrap(), duration);
+
+    let start = Instant::now();
+    let res_ex2 = ex2(&file);
+    let duration = start.elapsed();
+    println!("ex2: {} (computed in {:?})", res_ex2.unwrap(), duration);
 }
