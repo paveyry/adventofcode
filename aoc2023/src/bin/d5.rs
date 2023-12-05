147166da4caeb9f40ea35224303f17cf212af9aa
Pierre-Alexandre VEYRY <pa.veyry@gmail.com>
2023-12-05 14:45:09 +0000

    2023: d5: add solution

---
+++ aoc2023/src/bin/d5.rs
@@ -0,0 +1,160 @@
+use core::str::Lines;
+use std::fs;
+use std::time::Instant;
+
+use anyhow::{Error, Result};
+use itertools::Itertools;
+use rangemap::RangeInclusiveMap;
+
+fn fill_rangemap(mut lines: Lines<'_>) -> Result<RangeInclusiveMap<u64, u64>> {
+    let mut m = RangeInclusiveMap::new();
+    loop {
+        let l = lines.next();
+        if l.is_none() {
+            break;
+        }
+        let l = l.unwrap();
+        if l.is_empty() {
+            break;
+        }
+        let mut vals = l.split_whitespace();
+        let value = vals
+            .next()
+            .ok_or_else(|| Error::msg("bad map line"))?
+            .parse::<u64>()?;
+        let start = vals
+            .next()
+            .ok_or_else(|| Error::msg("bad map line"))?
+            .parse::<u64>()?;
+        let end = start
+            + vals
+                .next()
+                .ok_or_else(|| Error::msg("bad map line"))?
+                .parse::<u64>()?
+            - 1;
+        m.insert(start..=end, value)
+    }
+    Ok(m)
+}
+
+fn fill_maps(mut lines: Lines<'_>, maps: &mut [RangeInclusiveMap<u64, u64>; 7]) -> Result<()> {
+    let mut index = 0;
+    loop {
+        let l = lines.next();
+        if l.is_none() {
+            break;
+        }
+        let l = l.unwrap();
+        if l.is_empty() {
+            continue;
+        }
+        if l.ends_with(':') {
+            maps[index] = fill_rangemap(lines.clone())?;
+            index += 1;
+        }
+    }
+    Ok(())
+}
+
+fn ex1(file: &str) -> Result<u64> {
+    let mut lines = file.lines();
+    let seeds = lines.next().ok_or_else(|| Error::msg("empty file"))?[7..].split_whitespace();
+    let mut maps: [RangeInclusiveMap<u64, u64>; 7] = Default::default();
+    fill_maps(lines, &mut maps)?;
+
+    let mut min = u64::MAX;
+    for s in seeds {
+        let mut v = s.parse::<u64>()?;
+        for m in maps.iter() {
+            if let Some((k, val)) = m.get_key_value(&v) {
+                v = *val + (v - k.start());
+            }
+        }
+        if v < min {
+            min = v;
+        }
+    }
+    Ok(min)
+}
+
+fn ex2(file: &str) -> Result<u64> {
+    let mut lines = file.lines();
+    let seeds = lines.next().ok_or_else(|| Error::msg("empty file"))?[7..].split_whitespace();
+    let mut maps: [RangeInclusiveMap<u64, u64>; 7] = Default::default();
+    fill_maps(lines, &mut maps)?;
+
+    let mut min = u64::MAX;
+    for (start, count) in seeds.tuples() {
+        let start = start.parse::<u64>()?;
+        let end = start + count.parse::<u64>()? - 1;
+        for seed in start..=end {
+            let mut v = seed;
+            for m in maps.iter() {
+                if let Some((k, val)) = m.get_key_value(&v) {
+                    v = *val + (v - k.start());
+                }
+            }
+            if v < min {
+                min = v;
+            }
+        }
+    }
+    Ok(min)
+}
+
+fn main() {
+    let file = fs::read_to_string("./inputs/d5_1.txt").unwrap();
+
+    let start = Instant::now();
+    let res_ex1 = ex1(&file);
+    let duration = start.elapsed();
+    println!("ex1: {} (computed in {:?})", res_ex1.unwrap(), duration);
+
+    let start = Instant::now();
+    let res_ex2 = ex2(&file);
+    let duration = start.elapsed();
+    println!("ex2: {} (computed in {:?})", res_ex2.unwrap(), duration);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test() {
+        let input = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+        assert_eq!(35, ex1(input).unwrap());
+        assert_eq!(46, ex2(input).unwrap());
+    }
+}
